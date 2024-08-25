f7dcbcd98b85c4809403e75d3da0ea09efc9823e
Ethan Zhang <ethanzhang345@gmail.com>
2024-08-25 22:19:32 +1000

    Platelet sound effect

--- Platelet/ContentView.swift
+++ Platelet/ContentView.swift
@@ -7,74 +7,7 @@
 
 import SwiftUI
 
-//struct ContentView: View {
-//    var body: some View {
-//        VStack {
-////            Image(systemName: "globe")
-////                .imageScale(.large)
-////                .foregroundStyle(.tint)
-////            Text("Hello, world!")
-//            var clicked = false
-//            Button {
-//                        clicked = !clicked
-//                    } label: {
-//                        PlateletImage(clicked: clicked)
-//                    }
-//
-//        }
-//
-//    }
-//}
-//
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//            .previewInterfaceOrientation(.landscapeLeft)
-//    }
-//}
-
-
-//struct ContentView: View {
-//    @State private var clicked = false
-//    @State private var count = 0
-//
-//
-//    var body: some View {
-//        
-//        HStack {
-//            Text("Tap Count: \(count)") // Display the tap count
-//                .font(.largeTitle)
-//                .padding()
-//                .allowsHitTesting(/*@START_MENU_TOKEN@*/false/*@END_MENU_TOKEN@*/)
-//            PlateletImage(clicked: clicked)
-//                .onLongPressGesture(minimumDuration: 1.0, pressing: { pressing in
-//                                // This closure will be called when the gesture state changes
-//                                clicked = pressing
-//                    print("Long press started")
-//
-//                            }) {
-//                                // This closure will be called when the long press gesture is completed
-//                                print("Long press completed")
-//                            }
-////                    .gesture(
-////                        LongPressGesture(minimumDuration: 5 )
-////                            .onChanged({ _ in
-////                                clicked = true
-////                                count+=1
-////                                print("click hold")
-////
-////                            })
-////                            .onEnded({ _ in
-////                                clicked = false
-////                                print("click release")
-////                            })
-////                )                
-//                    .ignoresSafeArea()
-//        }
-//
-//        
-//    }
-//}
+import AVFoundation
 struct ContentView: View {
     @State private var isPressed = false
     @State private var touchCount = 0
@@ -91,6 +24,7 @@ struct ContentView: View {
                             if !isPressed {
                                 isPressed = true
                                 touchCount += 1
+                                SoundManager.shared.playTapSound()
                             }
                         }
                         .onEnded { _ in
@@ -99,38 +33,29 @@ struct ContentView: View {
                 )
             
             Text("Poke Count: \(touchCount)")
-                .font(.system(.largeTitle, design: .rounded))
-                .fontWeight(.medium)
-                .foregroundStyle(Color(red: 0.599, green: 0.412, blue: 0.409))
-                .padding(.bottom, 17.238)
+                           .font(.system(.largeTitle, design: .rounded))
+                           .fontWeight(.medium)
+                           .foregroundStyle(Color(red: 0.599, green: 0.412, blue: 0.409))
+                           .padding(.bottom, 17.238)
         }
     }
 }
-
-
-struct PlateletImage: View {
-    var clicked: Bool
-    var count = 0
-    var body: some View {
-   
-            if clicked {
-                Image("platelet image2")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .ignoresSafeArea()
-            } else {
-                Image("platelet image")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .ignoresSafeArea()
-            
+class SoundManager {
+    static let shared = SoundManager()
+    private var audioPlayer: AVAudioPlayer?
+    
+    private init() {
+        if let soundURL = Bundle.main.url(forResource: "poke", withExtension: "mp3") {
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+                audioPlayer?.prepareToPlay()
+            } catch {
+                print("Error loading sound file")
+            }
         }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-            .previewInterfaceOrientation(.landscapeLeft)
+    
+    func playTapSound() {
+        audioPlayer?.play()
     }
 }
